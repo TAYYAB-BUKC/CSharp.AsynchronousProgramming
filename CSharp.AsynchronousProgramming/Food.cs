7c39553eddf11897f0a900ee8c9f20380d489b4e
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-09-19 23:06:13 +0500

    Food and some dishes classes implemented.

---
+++ CSharp.AsynchronousProgramming/Food.cs
@@ -0,0 +1,26 @@
+﻿namespace CSharp.AsynchronousProgramming
+{
+	public abstract class Food
+	{
+		readonly TimeSpan _cookTime;
+		public string Name { get; set; }
+
+		protected Food(TimeSpan cookTime)
+		{
+			_cookTime = cookTime;
+			Name = GetType().Name;
+		}
+
+		public async Task Cook()
+		{
+			Console.WriteLine($"Cooking {Name}");
+			await Task.Delay(_cookTime);
+			Console.WriteLine($"{Name} cooking completed");
+		}
+	}
+
+	public class Turkey() : Food(TimeSpan.FromSeconds(5));
+	public class MashedPotatoes() : Food(TimeSpan.FromSeconds(2));
+	public class Gravy() : Food(TimeSpan.FromSeconds(1));
+	public class Stuffing() : Food(TimeSpan.FromSeconds(2));
+}
