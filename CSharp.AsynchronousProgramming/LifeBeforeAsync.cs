75b1b554e953bb0408890cd64826a6a6b1df8228
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-09-19 23:21:43 +0500

    Call Back Hell example before Async/Await implemented.

---
+++ CSharp.AsynchronousProgramming/LifeBeforeAsync.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharp.AsynchronousProgramming
+{
+	public class LifeBeforeAsync
+	{
+		public static async Task RunExample()
+		{
+			#region Call Back Hell Example Before Async/Await
+			Console.WriteLine("Cooking Started");
+			
+			var turkey = new Turkey();
+			turkey.Cook()
+				  .ContinueWith((prevTask) =>
+				  {
+					  var gravy = new Gravy();
+					  gravy.Cook()
+						   .ContinueWith((prevTask) =>
+						   {
+							   Console.WriteLine("Ready to eat");
+						   });
+				  });
+			#endregion
+		}
+	}
+}
